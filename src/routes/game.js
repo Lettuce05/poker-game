7bd2fd91e082f347a767878f205ea0795a53db87
Lettuce05 <romaine@unlv.nevada.edu>
2022-05-07 12:33:08 -0700

    Added card svg

--- src/routes/game.js
+++ src/routes/game.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Deck from '../Utils/Deck'
 import Button from '../components/Button/Button'
 import { Link } from 'react-router-dom'
+import Ace from '../assets/cards/SA.svg'
 import '../components/Game/Game.css'
 
 export default function Game() {
@@ -12,13 +13,18 @@ export default function Game() {
     
     return (
         <main>
+            {/* navigation */}
             <nav className="game-nav">
                 <Link className="btn btn-orange" to="/settings">Settings</Link>
                 <Link className="btn btn-orange" to="/">Home</Link>
             </nav>
-            {/* navigation */}
+
             {/* current hand */}
+            <section style={{padding: "1rem"}}>
+                <img src={Ace} alt="card svg" style={{width: "100px", height: "auto", objectFit: "contain"}}/>
+            </section>
             {/* buttons */}
+            <section></section>
         </main>
     )
 }
